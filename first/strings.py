d01eede55a320c1a9fe90f59d0554c2f4ca44b0a
NadyaKulbei <6886437nadik@gmail.com>
2022-10-02 21:35:58 +0300

    First commit1

--- first/strings.py
+++
@@ -1,5 +0,0 @@
-s = int(input())
-if s == 1:
-    print ('dkkfjh')
-else:
-    print('No')
--- first/task_3.py
+++
@@ -1,7 +0,0 @@
-s = input("Введите строку, а я выведу нужное ")
-i = ""
-for c in s:
-    if c in i or c == ' ':
-        continue
-    i = i + c
-print(i)
