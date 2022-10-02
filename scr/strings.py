189b558b388df730bab67c3ba4b11c7dea66172f
NadyaKulbei <6886437nadik@gmail.com>
2022-10-02 21:45:58 +0300

    First commit1

---
+++ scr/strings.py
@@ -0,0 +1,5 @@
+s = int(input())
+if s == 1:
+    print ('dkkfjh')
+else:
+    print('No')
---
+++ scr/task_1.py
@@ -0,0 +1,14 @@
+s = input("Введите строку, а я выведу количество ")
+i = 0
+little_i = 0
+import string
+#string.ascii_uppercase
+#string.asccii_lowcase
+for c in s:
+    if c in string.ascii_uppercase:
+        i = i+1
+    if c in string.ascii_lowercase:
+        little_i  = little_i +1
+print("количество маленьких букв ", little_i)
+print("количество больших букв ", i)
+print('jj')
